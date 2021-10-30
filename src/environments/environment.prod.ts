911c248f2ee89ad23516babd144c7dfd9630380d
YarnNoir <billyoneid@outlook.co.id>
2021-10-30 14:42:37 +0000

    initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'dsiworld',
+    appId: '1:219980495763:web:82ac44eb5aed44c047c666',
+    storageBucket: 'dsiworld.appspot.com',
+    apiKey: 'AIzaSyDjjVRPopm2Qp1OjrSTnt9DwndmC7Lqb3U',
+    authDomain: 'dsiworld.firebaseapp.com',
+    messagingSenderId: '219980495763',
+    measurementId: 'G-XNRDMH9L6M',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'dsiworld',
+    appId: '1:219980495763:web:82ac44eb5aed44c047c666',
+    storageBucket: 'dsiworld.appspot.com',
+    apiKey: 'AIzaSyDjjVRPopm2Qp1OjrSTnt9DwndmC7Lqb3U',
+    authDomain: 'dsiworld.firebaseapp.com',
+    messagingSenderId: '219980495763',
+    measurementId: 'G-XNRDMH9L6M',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
